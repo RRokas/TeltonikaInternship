c05708741ba050eda91c4e0525fb4384eb4856ce
RRokas <rokas.ringaila@gmail.com>
2023-07-12 23:41:20 +0300

    simple metadata table implementation

--- CLI/Program.cs
+++ CLI/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using Core.Entities;
 using Spectre.Console;
 
@@ -18,7 +19,36 @@ namespace CLI
             var table = new ParameterComparisonTable(comparison);
             //table.ApplyResultFilter(ComparisonResult.Removed);
             table.ApplyParameterNameFilter("10");
+            var metadataTable = MetadataTable(comparison);
+            AnsiConsole.Write(metadataTable);
             AnsiConsole.Write(table.BuildTable());
+        }
+
+        public static Grid MetadataTable(Comparison comparison)
+        {
+            var sourceTable = new Table().Title("Source");
+            var targetTable = new Table().Title("Target");
+            sourceTable.AddColumn("Parameter");
+            sourceTable.AddColumn("Value");
+            targetTable.AddColumn("Parameter");
+            targetTable.AddColumn("Value");
+
+            foreach (var parameter in comparison.Source.Parameters.Where(x => x.IsMetadata))
+            {
+                sourceTable.AddRow(parameter.Id.ToString() ?? string.Empty, parameter.Value.ToString() ?? string.Empty);
+            }
+
+            foreach (var parameter in comparison.Target.Parameters.Where(x => x.IsMetadata))
+            {
+                targetTable.AddRow(parameter.Id.ToString() ?? string.Empty, parameter.Value.ToString() ?? string.Empty);
+            }
+            
+            // create panel with two tables
+            var grid = new Grid();
+            grid.AddColumn(new GridColumn().NoWrap());
+            grid.AddColumn(new GridColumn().NoWrap());
+            grid.AddRow(sourceTable, targetTable);
+            return grid;
         }
     }
 }
