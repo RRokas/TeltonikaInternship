f3e48c2f7d074a1a8247ff99df8fd8ec77947125
RRingaila <r.ringaila@mttc.eu>
2023-07-13 19:57:22 +0300

    simple temporary ui added

--- CLI/Program.cs
+++ CLI/Program.cs
@@ -18,15 +18,56 @@ namespace CLI
             
             var tableBuilder = new TableBuilder(comparison);
             
+            var choice = AskComparisonType();
+            if (choice == MenuChoice.FilterById)
+            {
+                var filterString = AnsiConsole.Ask<string>("Enter substring to search for");
+                tableBuilder.ApplyParameterNameFilter(filterString);
+            } else if(choice == MenuChoice.FilterByResult)
+            {
+                var filterString = AnsiConsole.Prompt(
+                    new SelectionPrompt<ComparisonResult>()
+                        .Title("Select result to filter by")
+                        .AddChoices(new[] {
+                            ComparisonResult.Unchanged,
+                            ComparisonResult.Modified,
+                            ComparisonResult.Added,
+                            ComparisonResult.Removed
+                        }));
+                tableBuilder.ApplyResultFilter(filterString);
+            }
+            
             var comparisonTable = tableBuilder.BuildComparisonTable();
             var metadataTables = tableBuilder.BuildMetadataTables();
            
             var rootGrid = new Grid().AddColumn();
             var metadataGrid = new Grid().AddColumns(2).AddRow(metadataTables.source, metadataTables.target);
-            rootGrid.AddRow(metadataGrid);
-            rootGrid.AddRow(comparisonTable.Expand());
+
+            rootGrid.AddRow(metadataGrid).Expand();
+            rootGrid.AddRow(summaryTable.Expand());
+            rootGrid.AddRow(new Padder(comparisonTable.Expand()));
 
             AnsiConsole.Write(rootGrid);
+        }
+
+        static MenuChoice AskComparisonType()
+        {
+            return AnsiConsole.Prompt(
+                new SelectionPrompt<MenuChoice>()
+                    .Title("Action")
+                    .AddChoices(new[] {
+                        MenuChoice.ShowFullComparison,
+                        MenuChoice.FilterById,
+                        MenuChoice.FilterByResult
+                    }));
+            
+        }
+        
+        private enum MenuChoice
+        {
+            ShowFullComparison,
+            FilterById,
+            FilterByResult
         }
     }
 }
