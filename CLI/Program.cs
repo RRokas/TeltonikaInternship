91fcf503481682f7188c225a306031e0ab94b097
RRingaila <r.ringaila@mttc.eu>
2023-07-13 13:00:44 +0300

    Renamed ParameterComparisonTable.cs to TableBuilder.cs to suit the function of the class better. Moved metadata table creation out of Program.cs Improved output to the console, main comparison table expands to match the width of metadata tables. Filtering of parameter comparison is now with comparison objects, not formatted strings.

--- CLI/Program.cs
+++ CLI/Program.cs
@@ -13,42 +13,23 @@ namespace CLI
         {
             var sourceConfigFile = new FileInfo(args[0]);
             var targetConfigFile = new FileInfo(args[1]);
+            
             var source = new DeviceConfiguration(sourceConfigFile);
             var target = new DeviceConfiguration(targetConfigFile);
-            var comparison = new Comparison(source, target);
-            var table = new ParameterComparisonTable(comparison);
-            //table.ApplyResultFilter(ComparisonResult.Removed);
-            table.ApplyParameterNameFilter("10");
-            var metadataTable = MetadataTable(comparison);
-            AnsiConsole.Write(metadataTable);
-            AnsiConsole.Write(table.BuildTable());
-        }
-
-        public static Grid MetadataTable(Comparison comparison)
-        {
-            var sourceTable = new Table().Title("Source");
-            var targetTable = new Table().Title("Target");
-            sourceTable.AddColumn("Parameter");
-            sourceTable.AddColumn("Value");
-            targetTable.AddColumn("Parameter");
-            targetTable.AddColumn("Value");
-
-            foreach (var parameter in comparison.Source.Parameters.Where(x => x.IsMetadata))
-            {
-                sourceTable.AddRow(parameter.Id.ToString() ?? string.Empty, parameter.Value.ToString() ?? string.Empty);
-            }
-
-            foreach (var parameter in comparison.Target.Parameters.Where(x => x.IsMetadata))
-            {
-                targetTable.AddRow(parameter.Id.ToString() ?? string.Empty, parameter.Value.ToString() ?? string.Empty);
-            }
             
-            // create panel with two tables
-            var grid = new Grid();
-            grid.AddColumn(new GridColumn().NoWrap());
-            grid.AddColumn(new GridColumn().NoWrap());
-            grid.AddRow(sourceTable, targetTable);
-            return grid;
+            var comparison = new Comparison(source, target);
+            
+            var tableBuilder = new TableBuilder(comparison);
+            
+            var comparisonTable = tableBuilder.BuildComparisonTable();
+            var metadataTables = tableBuilder.BuildMetadataTables();
+           
+            var rootGrid = new Grid().AddColumn();
+            var metadataGrid = new Grid().AddColumns(2).AddRow(metadataTables.source, metadataTables.target);
+            rootGrid.AddRow(metadataGrid);
+            rootGrid.AddRow(comparisonTable.Expand());
+
+            AnsiConsole.Write(rootGrid);
         }
     }
 }
--- CLI/TableBuilder.cs
+++ CLI/TableBuilder.cs
@@ -5,28 +5,30 @@ using Spectre.Console;
 
 namespace CLI
 {
-    public class ParameterComparisonTable
+    public class TableBuilder
     {
-        private List<ParameterComparisonRow> Rows {get;}
+        private Comparison Comparison {get; }
         private List<ParameterComparisonRow> FilteredRows {get; set;}
 
-        public ParameterComparisonTable(Comparison comparison)
+        public TableBuilder(Comparison comparison)
         {
-            Rows = comparison.Results.Select(CreateTableRow).ToList();
-            FilteredRows = Rows;
+            Comparison = comparison ?? throw new System.ArgumentNullException(nameof(comparison));
+            FilteredRows = Comparison.Results.Select(CreateTableRow).ToList();
         }
         
         public void ApplyParameterNameFilter(string subStringToSearchFor)
         {
-            FilteredRows = Rows.Where(x=>x.ParameterName.StartsWith(subStringToSearchFor)).ToList();
+            FilteredRows = Comparison.Results.Where(x=>
+                    x.Source.Id.ToString().Contains(subStringToSearchFor) || x.Target.Id.ToString().Contains(subStringToSearchFor))
+                .Select(CreateTableRow).ToList();
         }
         
         public void ApplyResultFilter(ComparisonResult result)
         {
-            FilteredRows = Rows.Where(x=>x.Result == result.ToString()).ToList();
+            FilteredRows = Comparison.Results.Where(x=>x.Result == result).Select(CreateTableRow).ToList();
         }
 
-        public Table BuildTable()
+        public Table BuildComparisonTable()
         {
             var table = new Table().Title("Parameter comparison");
             table.AddColumn("Parameter");
@@ -42,12 +44,35 @@ namespace CLI
             return table;
         }
 
+        public (Table source, Table target) BuildMetadataTables()
+        {
+            var sourceTable = new Table().Title("Source");
+            var targetTable = new Table().Title("Target");
+            sourceTable.AddColumn("Parameter");
+            sourceTable.AddColumn("Value");
+            targetTable.AddColumn("Parameter");
+            targetTable.AddColumn("Value");
+
+            foreach (var parameter in Comparison.Source.Parameters.Where(x => x.IsMetadata))
+            {
+                sourceTable.AddRow(parameter.Id.ToString() ?? string.Empty, parameter.Value.ToString() ?? string.Empty);
+            }
+
+            foreach (var parameter in Comparison.Target.Parameters.Where(x => x.IsMetadata))
+            {
+                targetTable.AddRow(parameter.Id.ToString() ?? string.Empty, parameter.Value.ToString() ?? string.Empty);
+            }
+            
+            return (sourceTable, targetTable);
+        }
+
         private ParameterComparisonRow CreateTableRow(ConfigurationParameterComparison comparisonResult)
         {
             var parameterName = comparisonResult.Source?.Id.ToString() ?? comparisonResult.Target?.Id.ToString();
             var sourceValue = comparisonResult.Source?.Value.ToString() ?? "";
             var targetValue = comparisonResult.Target?.Value.ToString() ?? "";
             var comparisonResultString = comparisonResult.Result.ToString();
+            
             return new ParameterComparisonRow
             {
                 ParameterName = ApplyFormatting(parameterName, comparisonResult.Result),
