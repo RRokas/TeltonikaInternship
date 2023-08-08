e11bf07bcd558d5b455c27e9867fab15c125971b
RRokas <rokas.ringaila@gmail.com>
2023-08-09 00:49:57 +0300

    Refactor code and add MudBlazor styling

--- src/API/Program.cs
+++ src/API/Program.cs
@@ -1,9 +1,5 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using NLog;
@@ -15,7 +11,7 @@ namespace API
     {
         public static void Main(string[] args)
         {
-            var logger = NLog.LogManager.Setup().LoadConfigurationFromAppSettings().GetCurrentClassLogger();
+            var logger = LogManager.Setup().LoadConfigurationFromAppSettings().GetCurrentClassLogger();
 
             try
             {
